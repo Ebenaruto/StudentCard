f8a91b5b672b8ca2cea16a08fa5d1a26b06a893e
Ebenaruto <omartial30@gmail.com>
2020-07-17 17:29:10 +0000

    authentification interface

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Student_Card</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/materialize.min.css">
+        <link rel="stylesheet" href="assets">
 
         <!-- Styles -->
         <style>
